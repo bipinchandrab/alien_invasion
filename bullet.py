75054e9e7cb4007e3cc467336321cfa31c725daf
bipin chandra <bipin.b.chandra@gmail.com>
2021-05-21 14:28:19 +1000

    bullet module

--- bullet.py
+++ bullet.py
@@ -1,13 +1,14 @@
 import pygame
-   from pygame.sprite import Sprite
+from pygame.sprite import Sprite
 
-   class Bullet(Sprite):
 
-       def __init__(self, ai_game):
-           super().__init__()
-           self.screen = ai_game.screen
-           self.settings = ai_game.settings
-           self.color = self.settings.bullet_color
-           self.rect = pygame.Rect(0, 0, self.settings.bullet_width, self.settings.bullet_height)
-           self.rect.midtop = ai_game.ship.rect.midtop
-           self.y = float(self.rect.y)
+class Bullet(Sprite):
+
+    def __init__(self, ai_game):
+        super().__init__()
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+        self.color = self.settings.bullet_color
+        self.rect = pygame.Rect(0, 0, self.settings.bullet_width, self.settings.bullet_height)
+        self.rect.midtop = ai_game.ship.rect.midtop
+        self.y = float(self.rect.y)
