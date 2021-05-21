d3a57a332bbd17ea4f1f93a09c7e618e091847aa
bipin chandra <bipin.b.chandra@gmail.com>
2021-05-21 15:09:04 +1000

    firing bullets and minor changes

--- settings.py
+++ settings.py
@@ -2,9 +2,10 @@ class Settings:
     def __init__(self):
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (230,230,230)
+        self.bg_color = (0,0,0)
         self.ship_speed = 1.5
         self.bullet_speed = 1.0
         self.bullet_width = 3
         self.bullet_height = 15
-        self.bullet_color = (60, 60, 60)
+        self.bullet_color = (255, 100, 100)
+        self.bullets_allowed = 3
